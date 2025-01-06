1956dde0e2ab65bf0489595dc371a9d9d21189cf
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-06 19:03:26 +0700

    Add Manipulation on the matrix

--- Matrix/Matrix.py
+++ Matrix/Matrix.py
@@ -1,3 +1,11 @@
 import numpy as ny
 A=ny.array([[1,2,3],[4,5,6]])
 print(A)
+print("Các thao tác đơn giản trong ma trận")
+print(A[0,0])
+print(A[:,0])
+print(A[0,:])
+print(A[1,1])
+print(A[:,1])
+print(A[1,:])
+# Phía trước là hàng, phía sau là cột
