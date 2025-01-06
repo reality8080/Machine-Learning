e8636b7f73f0947ee42237ca143a391c2af7cf4f
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-06 19:51:00 +0700

    Add muls

--- Matrix/Multiplication/Matrix.py
+++ Matrix/Multiplication/Matrix.py
@@ -4,3 +4,9 @@ A=np.array([[1,2,3],[4,5,6],[7,8,9]])
 B=np.array([[1,2,3],[4,5,6],[7,8,9]])
 print("Phép nhân cùng M*N:\n",A.dot(B))
 # Lưu ý chỉ có thể cộng với các ma trận cùng là M*N
+n=int(input("Hãy nhập n mong muốn: "))
+print("Phép nhân với một số: ", A*n)
+
+C=np.array([[1,2,3],[4,5,6],[7,8,9]])
+DD=np.array([1,2,3])
+print("Phép nhân giữa ma trận và vector:\n",A*B)
