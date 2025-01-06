d2ffc434494a037f5defa6cd9b75ccf1fbbfdf1d
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-06 20:00:43 +0700

    Divi, Mul

--- Matrix/Multiplication/Matrix.py
+++ Matrix/Multiplication/Matrix.py
@@ -10,3 +10,7 @@ print("Phép nhân với một số: ", A*n)
 C=np.array([[1,2,3],[4,5,6],[7,8,9]])
 DD=np.array([1,2,3])
 print("Phép nhân giữa ma trận và vector:\n",A*B)
+
+# A=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# B=np.array([[1,2,3],[4,5,6]])
+# print("Phép nhân khácác M*N:\n",A.dot(B))
